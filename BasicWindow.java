98c335113b15f54e623e8e2cb727920991e8d89a
Gavin Rea <gavin@reaworld.org>
2013-02-13 10:16:49 -0400

    Tree class and key listeners

--- BasicWindow.java
+++ BasicWindow.java
@@ -6,21 +6,22 @@ import java.io.*;
 
 public class BasicWindow extends JFrame 
 {
+	//all initial values for frame and world size
 	public static final int TILE_SIZE = 50;
 	public static final int VIEW_SIZE = 11;
 	public static final int SCREEN_SIZE = TILE_SIZE * VIEW_SIZE;
 	public static final int NUM_WORLD_ROWS = 1000;
 	public static final int NUM_WORLD_COLS = 1000;
-
+	//create world to specifications
 	private Tile[][] world = new Tile[NUM_WORLD_ROWS][NUM_WORLD_COLS];
-
+	//starting location of character
 	private int charRow = 5;
 	private int charCol = 5;
 
 	// data structure 
 	class MyCanvas extends JComponent 
 	{
-		
+
 		public void paint(Graphics g)
 		{
 			super.paint(g);
@@ -28,12 +29,15 @@ public class BasicWindow extends JFrame
 			for(int row = charRow - 5; row <= charRow + 5; row++){
 				int y = 0;
 				for(int col = charCol - 5; col <= charCol + 5; col++){
+					//if there's a tile there, paint it
 					if(world[row][col] != null){
 						world[row][col].draw(g, x , y);
+						//draw character over its location
 						if(col == charCol && row == charRow){
+							//character specifications
 							Color skin = new Color(240, 210, 175);
 							g.setColor(skin);
-							g.fillRoundRect(x + 20, y + 20, 10, 20, 5, 5);
+							g.fillRoundRect(x + 15, y + 20, 20, 20, 5, 5);
 							g.fillOval(x + 10, y + 10, 30, 20);
 						}
 					}
@@ -43,87 +47,118 @@ public class BasicWindow extends JFrame
 			}
 		}
 	}
+	//we need a class keylistener, so we can make it an object and pass it to Jcomponent
 	public class ListenForKey implements KeyListener{
 		public void keyPressed(KeyEvent k){
-			
-		}
-		public void keyReleased(KeyEvent k){
-			
-		}
-		public void keyTyped(KeyEvent k){
-			char c = k.getKeyChar();
-			System.out.println("The key pressed was " + c);
-			if(c == 'a'){
+			while(keyPressed){
+			int code = k.getKeyCode();
+			//print out the key
+			System.out.println("The key pressed was " + code);
+			//asdw controlls
+			if(code == VK_A){
 				charRow --;
 			}
-			else if(c == 'd'){
+			else if(code == VK_D){
 				charRow++;
 			}
-			else if(c == 'w'){
+			else if(code == VK_W){
 				charCol--;
 			}
-			else if(c == 's'){
+			else if(code == VK_S){
 				charCol++;
 			}
 			repaint();
 		}
+			//wait 1 second
 	}
-	// constructor
-	public BasicWindow() throws FileNotFoundException
-	{
-		super("My fancy title");
-		MyCanvas mc = new MyCanvas();
-		addKeyListener(new ListenForKey());
-		mc.setPreferredSize(new Dimension(TILE_SIZE * VIEW_SIZE, TILE_SIZE * VIEW_SIZE));
-		add(mc);
-		pack();
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		// JButton jb = new JButton("Click Me!");
-		// add(jb);
-		for(int i = 0; i < NUM_WORLD_ROWS;i++){
-			for(int j = 0; j < NUM_WORLD_ROWS;j++){
-				world[i][j] = new BlankTile();
-			}
+	public void keyReleased(KeyEvent k){
+
+	}
+	//controller functions
+	public void keyTyped(KeyEvent k){
+		char c = k.getKeyChar();
+		//print out the key
+		System.out.println("The key pressed was " + c);
+		//asdw controlls
+		if(c == 'a'){
+			charRow --;
 		}
-		readWorld("world"); 
+		else if(c == 'd'){
+			charRow++;
+		}
+		else if(c == 'w'){
+			charCol--;
+		}
+		else if(c == 's'){
+			charCol++;
+		}
 		repaint();
-		setVisible(true);
 	}
-	public void readWorld(String fileName) throws FileNotFoundException{
-		File f = new File(fileName);
-		if(f.isFile()){
-			Scanner s = new Scanner(f);
-			int numLines = s.nextInt();
-			for(int i = 0; i < numLines; i++){
-				int row = s.nextInt();
-				int col = s.nextInt();
-				int height = s.nextInt();
-				int width = s.nextInt();
-				Tile t = new BlankTile();
-				String terrainType = s.next();
-				if(terrainType.equals("forest")){
-					t = new ForestTile();
-				}
-				else if(terrainType.equals("water")){
-					t = new WaterTile();
-				}
-				else if(terrainType.equals("dirt")){
-					t = new DirtTile();
-				}
-				for(int x = row; x < row + width; x++){
-					for(int y = col; y < col + height; y++){
-						world[x][y] = t;
-						System.out.println("creating "+terrainType+" at " + row + ", "+ col);
-
-					}
-				}
-			}
-			s.close();
+}
+// constructor for window
+public BasicWindow() throws FileNotFoundException
+{
+	super("My fancy title");
+	MyCanvas mc = new MyCanvas();
+	addKeyListener(new ListenForKey());
+	mc.setPreferredSize(new Dimension(TILE_SIZE * VIEW_SIZE, TILE_SIZE * VIEW_SIZE));
+	add(mc);
+	pack();
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	//fill world with blank tiles so we won't get null pointer exceptions
+	for(int i = 0; i < NUM_WORLD_ROWS;i++){
+		for(int j = 0; j < NUM_WORLD_ROWS;j++){
+			world[i][j] = new BlankTile();
 		}
 	}
+	//read in the world
+	readWorld("world"); 
+	//paint the world
+	repaint();
+	setVisible(true);
+}
+//Readworld Function
+public void readWorld(String fileName) throws FileNotFoundException{
+	File f = new File(fileName);
+	if(f.isFile()){
+		Scanner s = new Scanner(f);
+		//the first number in the file is the number of lines to read
+		int numLines = s.nextInt();
+		for(int i = 0; i < numLines; i++){
+			//upper left hand corner
+			int row = s.nextInt();
+			int col = s.nextInt();
+			//how many tiles wide/heigh
+			int height = s.nextInt();
+			int width = s.nextInt();
+			//create a tile which will run a gauntlet of if statements to be assigned
+			Tile t = new BlankTile();
+			String terrainType = s.next();
+			if(terrainType.equals("forest")){
+				t = new ForestTile();
+			}
+			else if(terrainType.equals("water")){
+				t = new WaterTile();
+			}
+			else if(terrainType.equals("dirt")){
+				t = new DirtTile();
+			}
+			//set the world array point equal to the tile
+			for(int x = row; x < row + width; x++){
+				for(int y = col; y < col + height; y++){
+					world[x][y] = t;
+					//log to console
+					System.out.println("creating "+terrainType+" at " + row + ", "+ col);
 
-	public static void main(String[] args) throws FileNotFoundException
-	{
-		BasicWindow myWindow = new BasicWindow();
+				}
+			}
+		}
+		s.close();
 	}
+}
+//main method
+public static void main(String[] args) throws FileNotFoundException
+{
+	BasicWindow myWindow = new BasicWindow();
+}
 }
--- Tile.java
+++ Tile.java
@@ -1,19 +1,20 @@
 import java.awt.*;
-
-
+//superclass tile. Abstract so you have to make a type of one
 public abstract class Tile {
 	public abstract void draw(Graphics g, int x, int y);
 }
+//the default tile, which we will set the world to initially before assigning more specific values
 class BlankTile extends Tile{
 	public void draw(Graphics g, int x, int y){
 		g.setColor(Color.white);
 		g.fillRect(x, y, BasicWindow.TILE_SIZE, BasicWindow.TILE_SIZE);
 	}
 }
-
-class ForestTile extends Tile{
+//black background + tree
+class treeTile extends Tile{
 	public void draw(Graphics g, int x, int y){
-		g.setColor(Color.BLACK);
+		Color darkGreen = new Color(20, 90, 10);
+		g.setColor(darkGreen);
 		g.fillRect(x, y, BasicWindow.TILE_SIZE, BasicWindow.TILE_SIZE);
 		g.setColor(Color.GREEN);
 		int[] xVal = {x, x + 25, x + 49};
@@ -21,14 +22,31 @@ class ForestTile extends Tile{
 		g.fillPolygon(xVal, yVal, 3);
 	}
 }
-
+class ForestTile extends Tile{
+	public void draw(Graphics g, int x, int y){
+		Color darkGreen = new Color(20, 90, 10);
+		g.setColor(darkGreen);
+		g.fillRect(x, y, BasicWindow.TILE_SIZE, BasicWindow.TILE_SIZE);
+		g.setColor(new Color(90, 160, 35));
+		int[] aVal = {x, x + 25, x + 49};
+		int[] bVal = {y, y + 49, y};
+		g.fillPolygon(aVal, bVal, 3);
+		g.setColor(Color.GREEN);
+		int[] xVal = {x, x + 25, x + 49};
+		int[] yVal = {y + 49, y, y + 49};
+		g.fillPolygon(xVal, yVal, 3);
+		
+		
+	}
+}
+//blue tile
 class WaterTile extends Tile{
 	public void draw(Graphics g, int x, int y){
 		g.setColor(Color.BLUE);
 		g.fillRect(x,y, BasicWindow.TILE_SIZE, BasicWindow.TILE_SIZE);
 	}
 }
-
+//brown tile
 class DirtTile extends Tile{
 	public void draw(Graphics g, int x, int y){
 		Color brown = new Color(180, 110, 20);
